09c640c7907bb49b1572ee0bd291f236ba94c40a
ihzavip <39728148+ihzavip@users.noreply.github.com>
2022-05-04 20:54:29 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        medarkGray: "hsl(0, 0%, 63%)",
+        meblack: "hsl(0, 0%, 0%)",
+        mewhite: "hsl(0, 0%, 100%)",
+        meveryDarkGray: "hsl(0, 0%, 27%)",
+      },
+    },
+  },
+  plugins: [],
+};
